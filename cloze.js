eea3624ccd7bf603803d1ec9db7a69463a912336
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-02-24 18:16:18 -0500

    Finished backend homework done. The constructors are scope safe.

--- cloze.js
+++ cloze.js
@@ -1,35 +1,41 @@
-function ClozeFlashcard(text, cloze, err){
-	this.fullText = text;
-	this.cloze = cloze;
-	this.partial = text.replace(cloze, '...');
+function ClozeFlashcard(text, cloze){
 
-	this.clozeCards = function(){
+// Conditional that will allow users to create new flashcards without the use of "new" before the constructor name. 
+	if (this instanceof ClozeFlashcard) {
+		this.fullText = text;
+		this.cloze = cloze;
+		this.partial = text.replace(cloze, '...');
+ 
+ // Appends the cloze flashcards to a document.
+		this.clozeCards = function(){
 		
-		if (text.includes(cloze)){
+		// IF the cloze word is in the full text... 
+			if (text.includes(cloze)){
 
-			var fs = require('fs');
+				var fs = require('fs');
 
-			fs.appendFile("practice.txt", 
-				"\nCloze Flashcard: " + 
-				"\nQuestion: " + this.partial + 
-				"\nAnswer:" + cloze,
+				fs.appendFile("practice.txt", 
+					"\nCloze Flashcard: " + 
+					"\nQuestion: " + this.partial + 
+					"\nAnswer:" + cloze,
 
-			function(err) {
-				if (err) {
-					console.log(err);
-				}	
-				console.log("Done!");
-			});
-		} else {
-			console.log("Error!! This is not a valid flashcard!");
+				function(err) {
+					if (err) {
+						console.log(err);
+					}	
+					console.log("Done!");
+				});
+			// If the cloze word is NOT in the text it will return an error in the document.
+			} else {
+				console.log("Error!! This is not a valid flashcard!");
+			}
 		}
+	} else {
+		return new ClozeFlashcard(text, cloze);
 	}
 
-	if (text.includes(cloze)){
-		return;
-	} else {
-		console.log("Error!! This is not a valid flashcard!");
-	}
+	// Same conditional from above is specified so the user also sees that their submission was not valid.
+	if (!text.includes(cloze)) console.log("Error!! The full text does not include the cloze text!");
 }
 
 module.exports = ClozeFlashcard;
--- flash.js
+++ flash.js
@@ -1,42 +1,57 @@
+// Fulling in data from the cloze.js file in order to create cloze flashcards.
 var ClozeFlashcard = require("./cloze.js");
 
+// Constructor for creating a basic flashcard.
 function BasicFlashcard(front, back){
-	this.front = front;
-	this.back = back;
 
-	this.practiceCards = function(){
+// Conditional that will allow users to create new flashcards without the use of "new" before the constructor name. 
+	if (this instanceof BasicFlashcard) {
+		this.front = front;
+		this.back = back;
 
-		var fs = require('fs');
+	// Appends the front and back of the flashcard to the practice.txt file if needed.
+		this.practiceCards = function(){
 
-		fs.appendFile("practice.txt", 
-			"\nBasic Flashcard: " + 
-			"\nQuestion: " + front + 
-			"\nAnswer:" + back,
+			var fs = require('fs');
 
-		function(err) {
-			if (err) {
-				console.log(err);
-			}	
-			console.log("Done!");
-		});
+			fs.appendFile("practice.txt", 
+				"\nBasic Flashcard: " + 
+				"\nQuestion: " + front + 
+				"\nAnswer:" + back,
+
+			function(err) {
+				if (err) {
+					console.log(err);
+				}	
+				console.log("Done!");
+			});
+		}
+	} else {
+		return new BasicFlashcard(front, back);
 	}
 }
+
 // Tests for basic flashcards.
-var question1 = new BasicFlashcard("David Lynch is the creator of the Twin Peaks series.", "David Lynch");
+var question1 = BasicFlashcard("David Lynch is the creator of the Twin Peaks series.", "David Lynch");
 var question2 = new BasicFlashcard("Dale is Agent Cooper's first name.", "Dale");
 var question3 = new BasicFlashcard("Donna Hayward is Laura Palmer's best friend.", "Donna Hayward");
 var question4 = new BasicFlashcard("James Hurley is Laura Palmer's secret boyfriend.", "James Hurley");
 
-// question1.practiceCards();
-// question2.practiceCards();
-// question3.practiceCards();
-// question4.practiceCards();
-
 // Tests for new cloze flash cards.
-var clozeFlashcard1 = new ClozeFlashcard("David Lynch is the creator of the Twin Peaks series.", "David Lynch");
+var clozeFlashcard1 = ClozeFlashcard(" is the creator of the Twin Peaks series.", "David Lynch");
 var clozeFlashcard2 = new ClozeFlashcard("Dale is Agent Cooper's first name.", "Dale");
 var clozeFlashcard3 = new ClozeFlashcard("Donna Hayward is Laura Palmer's best friend.", "Donna Hayward");
 var clozeFlashcard4 = new ClozeFlashcard("James Hurley is Laura Palmer's secret boyfriend.", "James Hurley");
 
-console.log(clozeFlashcard1.partial);
+// Logging front and back of one flashcard.
+console.log("********************************");
+console.log("Basic Flashcard Example:"); 
+console.log("Front: " + question1.front);
+console.log("Back: " + question1.back);
+
+// Logging the front and back of a cloze flashcard.
+console.log("********************************");
+console.log("Cloze Flashcard Example:"); 
+console.log("Partial: " + clozeFlashcard1.partial);
+console.log("Cloze: " + clozeFlashcard1.cloze);
 
